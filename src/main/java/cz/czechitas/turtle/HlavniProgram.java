674b472f4b8a3a8db2a9e4c2e335cd080e503377
maja-nastassjadvorakova <dvorakova.maja12@gmail.com>
2025-04-24 19:08:33 +0200

    ukol2-část 1.1 - rovnostranny mini trojuhelnik

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,8 +2,12 @@ package cz.czechitas.turtle;
 
 import dev.czechitas.java1.turtle.engine.Turtle;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka = new Turtle();
+    private double initialPositionX;
+    private double initialPositionY;
 
     public static void main(String[] args) {
         new HlavniProgram().start();
@@ -11,6 +15,41 @@ public class HlavniProgram {
 
     public void start() {
         //TODO Tady bude kód pro kreslení želví grafiky.
+
+        Color ruzovaBarva = Color.pink;
+
+
+        nakresliRovnoStrannyTrojuhelnik(70.0, 120.0, Color.pink);
+
+        nakresliCtverec();
+
+    }
+
+    public void nakresliRovnoStrannyTrojuhelnik(double velikostStrany, double uhel, Color pink) {
+        zofka.turnRight(90);
+        zofka.setPenColor(pink);
+        for (int i = 0; i < 3; i++) {
+            zofka.move(velikostStrany);
+            zofka.turnRight(uhel);
+        }
+        novaKresba();
+
+    }
+
+    public void nakresliCtverec(){
+
+    }
+
+
+    public void novaKresba(){
+
+        initialPositionX = zofka.getX();
+        initialPositionY = zofka.getY();
+
+        zofka.setX(initialPositionX);
+        zofka.setY(initialPositionY);
+
+        zofka.turnRight(90);
     }
 
 }
